c8d9786faa221e7f789912b111fbf6967b0e66f3
Luis Morales <gnu936@gmail.com>
2025-06-29 17:44:35 -0500

    refactor comentario model

--- src/models/Comentario.ts
+++ src/models/Comentario.ts
@@ -2,9 +2,9 @@
 import { Schema, model, Document, Types } from 'mongoose';
 
 export interface IComentario extends Document {
-  id_sugerencia: Types.ObjectId;
   cantidad_estrellas_comentario: number;
   fecha_comentario: Date;
+  id_libro: Types.ObjectId;
   contenido_comentario: string;
   id_persona: Types.ObjectId;
   estado_comentario: boolean;
@@ -12,18 +12,18 @@ export interface IComentario extends Document {
 }
 
 const ComentarioSchema = new Schema<IComentario>({
-  id_sugerencia: { type: Schema.Types.ObjectId, ref: 'Sugerencia', required: true },
-  cantidad_estrellas_comentario: { type: Number, min: 1, max: 5 },
-  fecha_comentario: { type: Date, default: Date.now },
-  contenido_comentario: { type: String, required: true },
   id_persona: { type: Schema.Types.ObjectId, ref: 'Persona', required: true },
+  id_libro: { type: Schema.Types.ObjectId, ref: 'Libro', required: true },
+  cantidad_estrellas_comentario: { type: Number, min: 1, max: 5 },
+  contenido_comentario: { type: String, required: true },
+  fecha_comentario: { type: Date, default: Date.now },
   estado_comentario: { type: Boolean, default: true },
   reportado: [{ type: Schema.Types.ObjectId, ref: 'ReporteComentario' }]
 });
 
 // indexes
-ComentarioSchema.index({ id_sugerencia: 1, id_persona: 1 },
-  { unique: true, name: 'unique_sugerencia_persona' }
+ComentarioSchema.index({ id_libro: 1, id_persona: 1 },
+  { unique: true, name: 'unique_libro_persona' }
 );
 
 export const Comentario = model<IComentario>('Comentario', ComentarioSchema);
